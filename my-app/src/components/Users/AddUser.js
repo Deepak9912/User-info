db3253eeff15cf3ffab4e2d44bff46c3185f47f2
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-21 20:14:26 +0000

    add error modal

--- my-app/src/components/Users/AddUser.js
+++ my-app/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import react, {useState} from 'react';
 import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -29,6 +30,8 @@ const AddUser = (props) => {
     }
 
     return (
+        <div>
+        <ErrorModal title="An error occured" message="Something went wrong"/>
         <Card className={classes.input}>
             <form onSubmit={AddUserHandler}>
                 <label htmlFor="username">Username</label>
@@ -38,6 +41,7 @@ const AddUser = (props) => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
+        </div>
     )
 }
 
